9b0624c82840a1318272d059b04747935a2e1979
sselvapriyaa <selvapriyaphd@gmail.com>
2022-03-01 18:51:55 -0600

    modified details.html and created index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+//let age = "15";
+//console.log(typeof age);
+
+// Scoped Demonstartion
+/*let a = 5; b = 10;
+if(a > b){
+    let c = 50; //Scope of c is only within the if block
+}
+else{
+    let c = 100;
+}*/
+//console.log(c);
+
+//Reclaration is not allowed in let
+/*let user = "John";
+let user = "Mike";
+console.log(user); */
+
+//Reclaration is allowed in var
+var user = "John";
+var user = "Mike";
+console.log(user); 
+
+let userProfile; //Camelcase
+
+//Constant variable;
+const COLOR = "red";
+//COLOR = "blue";
+console.log(COLOR);
+console.log(5/10);
+console.log(5/0);
+console.log("Test"/0);
+
+//let a = 10, b = 20;
+//console.log(`Sum of a and b is: ${a+b}`);//use backtick from keyboard Expression is always followed by $
+/*console.log("Sum of a and b is: ${a+b}");*/
+
+//let age = 8 > 9;
+/*let age = null;*/
+//console.log(typeof age);
+
+let age = "Test";
+console.log(typeof(age));
+
+//console.log("Value of a is: " +a +" ,Value of b is: " +b);
+
+console.log(5 == "5"); //compare 5 as a string with "5"
+console.log(5 === "5"); //strick equals compare both datatype and var
+console.log("5" === "5");//Assignment
+console.log('5' === "5");
+
+let a = 10;
+console.log(++a); //pre increment
+//console.log(a++); //a++ (post increment): a = a+1;
+//a++;
+console.log(a); 
+
+
+
+
+
+
