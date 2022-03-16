87d2ef2dad69abc66336e5c973b5c790ce50b86d
sselvapriyaa <selvapriyaphd@gmail.com>
2022-03-16 00:46:37 -0500

    updated arraydestructuring

--- arraydestructuring.js
+++ arraydestructuring.js
@@ -1,38 +1,36 @@
-
 var sales = [
-    { item: "PS4 Pro", stock: 3, original: 399.99 },
-    { item: "Xbox One X", stock: 1, original: 499.99, discount: 0.1 },
-    { item: "Nintendo Switch", stock: 4, original: 299.99 },
-    { item: "PS2 Console", stock: 1, original: 299.99, discount: 0.8 },
-    { item: "Nintendo 64", stock: 2, original: 199.99, discount: 0.65 }
+    { item: 'PS4 Pro', stock: 3, original: 399.99 },
+    { item: 'Xbox One X', stock: 1, original: 499.99, discount: 0.1 },
+    { item: 'Nintendo Switch', stock: 4, original: 299.99 },
+    { item: 'PS2 Console', stock: 1, original: 299.99, discount: 0.8 },
+    { item: 'Nintendo 64', stock: 2, original: 199.99, discount: 0.65 }
   ];
-var result = [];
-sales.forEach(function (record) {
-    if (record.discount) {
-      var saleAmount = (record.original - record.original * record.discount);
+var saleitem = [];
+sales.forEach(function (listitem) {
+    if (listitem.discount) {
+      var saleAmount = (listitem.original - listitem.original * listitem.discount);
       var saleAmountrounded = Math.round((saleAmount + Number.EPSILON) * 100) /100;
-      var totalDiscount = (record.stock * saleAmountrounded);
+      var totalDiscount = (listitem.stock * saleAmountrounded);
       var totalDiscountrounded = Math.round((totalDiscount + Number.EPSILON) * 100) /100;
-     
-      result.push({       
-        item: record.item,
-        stock: record.stock,
-        original: record.original,
-        discount: record.discount,
-        sale: saleAmountrounded,    
-        total: totalDiscountrounded,       
+      saleitem.push({       
+        Item: listitem.item,
+        Stock: listitem.stock,
+        Original: listitem.original,
+        Discount: listitem.discount,
+        Sale: saleAmountrounded,    
+        Total: totalDiscountrounded,       
       });
     } else {
-      result.push({
-        item: record.item,
-        stock: record.stock,
-        original: record.original,
-        sale: record.original,
-        total: (record.stock * record.original),
+      saleitem.push({
+        Item: listitem.item,
+        Stock: listitem.stock,
+        Original: listitem.original,
+        Sale: listitem.original,
+        Total: (listitem.stock * listitem.original),
       });
     }
   });   
-  console.log(result);
+  console.log(saleitem);
 
 
 
